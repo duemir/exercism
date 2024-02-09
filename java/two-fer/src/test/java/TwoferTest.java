a531d16c7a7b686082b354911980f4c81e4f73fa
Denys Digtiar <duemir@gmail.com>
2024-02-09 15:03:13 +1000

    java-track two-fer exercise refreshed

--- java/two-fer/src/test/java/TwoferTest.java
+++ java/two-fer/src/test/java/TwoferTest.java
@@ -1,47 +1,34 @@
-import org.junit.Before;
-import org.junit.Ignore;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class TwoferTest {
 
     private Twofer twofer;
 
-    @Before
+    @BeforeEach
     public void setup() {
         twofer = new Twofer();
     }
 
     @Test
     public void noNameGiven() {
-        String input = null;
-        String expected = "One for you, one for me.";
-
-        assertEquals(expected, twofer.twofer(input));
+        assertThat(twofer.twofer(null))
+                .isEqualTo("One for you, one for me.");
     }
 
     @Test
     public void aNameGiven() {
-        String input = "Alice";
-        String expected = "One for Alice, one for me.";
-
-        assertEquals(expected, twofer.twofer(input));
+        assertThat(twofer.twofer("Alice"))
+                .isEqualTo("One for Alice, one for me.");
     }
 
     @Test
     public void anotherNameGiven() {
-        String input = "Bob";
-        String expected = "One for Bob, one for me.";
-
-        assertEquals(expected, twofer.twofer(input));
+        assertThat(twofer.twofer("Bob"))
+                .isEqualTo("One for Bob, one for me.");
     }
 
-    @Test
-    public void emptyStringGiven() {
-        String input = "";
-        String expected = "One for , one for me.";
-
-        assertEquals(expected, twofer.twofer(input) );
-    }
 }

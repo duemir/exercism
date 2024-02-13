f8e46fa41cd4cdc55d731a35a77a90bdc216a8e4
Denys Digtiar <duemir@gmail.com>
2024-02-13 13:00:44 +1000

    java-track lasagna exercise done

---
+++ java/lasagna/src/main/java/Lasagna.java
@@ -0,0 +1,16 @@
+public class Lasagna {
+    public int expectedMinutesInOven() {
+        return 40;
+    }
+
+    public int remainingMinutesInOven(int actual) {
+        return expectedMinutesInOven() - actual;
+    }
+
+    public int preparationTimeInMinutes(int layers) {
+        return layers * 2;
+    }
+    public int totalTimeInMinutes(int layers, int actualMinutes) {
+        return preparationTimeInMinutes(layers) + actualMinutes;
+    }
+}
